58a44c859cdc06c8dcafe1a611834291663d58ff
try2chng <rbell@student.21-school.ru>
2022-01-19 22:17:56 +0300

    add strncmp + echo(add print variable)

--- src/commands/echo.c
+++ src/commands/echo.c
@@ -1,5 +1,24 @@
 #include "minishell.h"
 
+static void	print_variable(char *s, char **env)
+{
+	size_t	i;
+	size_t	j;
+
+	s++;
+	i = 0;
+	while (s[i])
+		i++;
+	while (*env && ft_strncmp(s, *env, i))
+		env++;
+	if (!(*env))
+		return ;
+	j = 0;
+	while ((*env)[i + j + 1])
+		j++;
+	write(1, (*env + i + 1), j);
+}
+
 void	ms_cmd_execute_echo(t_shell *shell)
 {
 		size_t	flg_n;
@@ -7,14 +26,21 @@ void	ms_cmd_execute_echo(t_shell *shell)
 		size_t	j;
 
 		i = 0;
+		if (shell->cmd->argc < 2)
+			return ;
 		flg_n = ft_strcmp((shell->cmd->argv)[1], "-n") == 0;
-		while (++i + flg_n < shell->cmd->argc)
+		while (++i + flg_n < (size_t)shell->cmd->argc)
 		{
 			j = 0;
-			while ((shell->cmd->argv)[i + flg_n][j])
-				j++;
-			write(1, shell->cmd->argv[i + flg_n], j);
-			write(1, " ", 1);
+			if ((shell->cmd->argv)[i + flg_n][j] == '$')
+				print_variable((shell->cmd->argv)[i + flg_n], shell->envp);
+			else
+			{
+				while ((shell->cmd->argv)[i + flg_n][j])
+					j++;
+				write(1, shell->cmd->argv[i + flg_n], j);
+				write(1, " ", 1);
+			}
 		}
 		if (flg_n)
 			write(1, "\n", 1);
