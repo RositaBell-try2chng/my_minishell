96784c7176cafda122d34c6983ae8be2b881d0ea
try2chng <rbell@student.21-school.ru>
2022-02-06 17:26:34 +0300

    fix echo without arg and multi -n

--- src/commands/echo.c
+++ src/commands/echo.c
@@ -1,5 +1,17 @@
 #include "minishell.h"
 
+static size_t	skip_flg_n(t_cmd *cmd)
+{
+	size_t	i;
+	size_t	res;
+
+	res = 0;
+	i = 0;
+	while (++i < (size_t)cmd->argc && !ft_strcmp((cmd->argv)[i], "-n"))
+		res++;
+	return (res);
+}
+
 void	ms_cmd_execute_echo(t_shell *shell)
 {
 	size_t	flg_n;
@@ -7,19 +19,20 @@ void	ms_cmd_execute_echo(t_shell *shell)
 	size_t	j;
 
 	i = 0;
-	if (shell->cmd->argc < 2)
-		return ;
-	flg_n = ft_strcmp((shell->cmd->argv)[1], "-n") == 0;
-	while (++i + flg_n < (size_t)shell->cmd->argc)
+	if (shell->cmd->argc >= 2)
 	{
-		j = 0;
-		while ((shell->cmd->argv)[i + flg_n][j])
-			j++;
-		write(1, shell->cmd->argv[i + flg_n], j);
-		if (i + flg_n + 1 < (size_t)shell->cmd->argc)
-			write(1, " ", 1);
+		flg_n = skip_flg_n(shell->cmd);
+		while (++i + flg_n < (size_t) shell->cmd->argc)
+		{
+			j = 0;
+			while ((shell->cmd->argv)[i + flg_n][j])
+				j++;
+			write(1, shell->cmd->argv[i + flg_n], j);
+			if (i + flg_n + 1 < (size_t) shell->cmd->argc)
+				write(1, " ", 1);
+		}
 	}
-	if (!flg_n)
+	if (shell->cmd->argc < 2 || !flg_n)
 		write(1, "\n", 1);
 	ms_cmd_argv_free(shell->cmd);
 	ms_shell_destroy(shell);
